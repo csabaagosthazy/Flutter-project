e5674bc910c67e18ac22d0285f0207dfbd0ee6b1
David Fernandes <david.fernandes@bluewin.ch>
2021-12-17 16:05:17 +0100

    Create class Activity

---
+++ lib/data/activity.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/widgets.dart';
+
+class Activity extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
