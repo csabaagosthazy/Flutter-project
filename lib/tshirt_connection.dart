c0374969bef53986ee61c4ee703b55891c291a88
l4p1n <l4p1n@l4p1n.ch>
2021-12-17 11:52:02 +0100

    Handle the parsing of the datapoint string in the tshirt_data.dart file.
    Since this parsing will be used in multiple places, better define it once and use it as many times as needed.

--- lib/tshirt_connection.dart
+++ lib/tshirt_connection.dart
@@ -14,39 +14,19 @@ class TshirtConnection {
   ///
   /// Throws explicitly:
   /// - [Exception] when receiving a bad response from the server. Bad responses encompass:
-  ///   - insufficient items in the text response
   ///   - HTTP code not 200
   ///
   /// Throws implicitly:
   /// - when parsing text response items into an integer failed
   /// - when the client is unable to send an HTTP request to the server
+  /// - when the received string does not contain exactly four elements
   Future<TshirtData> getData() async {
     final http.Response response;
 
     response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
-      var responseItems = response.body.split(" ");
-
-      // The t-shirt returns [uptime] [heartbeat rate] [temperature] [humidity],
-      // if no error has occurred.
-      if(responseItems.length < 4) {
-        throw Exception("The t-shirt did not return 4 elements in the text response");
-      }
-
-      var tshirtUptime = responseItems[1];
-      var heartBeatRate = int.parse(responseItems[1]);
-      var temperature = int.parse(responseItems[2]);
-      var humidity = int.parse(responseItems[3]);
-
-      TshirtData data = TshirtData(
-          time: tshirtUptime,
-          heartFrequency: heartBeatRate,
-          temperature: temperature,
-          humidity: humidity
-      );
-
-      return data;
+      return TshirtData.fromString(response.body);
     } else {
       throw Exception('Unable to connect to the t-shirt server');
     }
--- lib/tshirt_data.dart
+++ lib/tshirt_data.dart
@@ -1,8 +1,11 @@
 class TshirtData {
-   String time;
-   int heartFrequency;
-   int temperature;
-   int humidity;
+  // The member variables will never be null since we pass by either constructors
+  // that initialize all members. If this claim doesn't hold... well, runtime exceptions.
+  // @see https://dart.dev/tools/diagnostic-messages
+  late String time;
+  late int heartFrequency;
+  late int temperature;
+  late int humidity;
 
   //Constructor
   TshirtData({
@@ -11,6 +14,30 @@ class TshirtData {
     required this.temperature,
     required this.humidity,
   });
+
+  /// Transforms a string containing a datapoint into a [TshirtData] instance
+  ///
+  /// The datapoint format is the following: "<t-shirt server uptime> <heartbeat rate> <temperature> <humidity>".
+  /// Throws [Exception] if the string does not contain exactly four items when split.
+  ///
+  /// Throws implicitly a FormatException if the data items expected to contain integers
+  /// are not parsable into integers.
+  TshirtData.fromString(String datapoints) {
+    List<String> items = datapoints.split(" ");
+
+    if (items.length != 4) {
+      throw Exception(
+          "Passed datapoint string must have exactly four items. " +
+          items.length.toString() +
+          " items found"
+      );
+    }
+
+    time = items[1];
+    heartFrequency = int.parse(items[1]);
+    temperature = int.parse(items[2]);
+    humidity = int.parse(items[3]);
+  }
 
   @override
   String toString() {
