2ddadd479bf770b4d59140c0b7673a37c3fa7ae7
l4p1n <l4p1n@l4p1n.ch>
2021-12-17 11:15:31 +0100

    Fix 'Firebase App not initialized' error

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_group2_tshirt_project/tshirt_data.dart';
@@ -15,7 +16,7 @@ void main() async {
   runApp(const MyApp());
 
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
